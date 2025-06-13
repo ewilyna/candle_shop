a7987b2aa2f8c663a6ecb6cf0fbac3a1f45748eb
ewilyna <161340295+ewilyna@users.noreply.github.com>
2025-06-13 17:16:20 +0300

    hosts for render

--- candle_shop/settings.py
+++ candle_shop/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-candle-shop-secret-key-for-development'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
